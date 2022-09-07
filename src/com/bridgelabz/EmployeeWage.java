6e80337b2effa7ddcad1ba0747094b09676d005d
Manikandan6394 <manikandan.ramesh6394@gmail.com>
2022-09-07 09:39:41 +0530

    Started Employee wage program

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,10 +1,27 @@
 package com.bridgelabz;
 
-public class EmployeeWageComputation {
+import java.util.Random;
 
-	public static void main(String[] args) {
-		
-
+public class EmployeeWage {
+//My code
+	
+	public void employeeIsPresentOrNot() {
+	
 	}
+	public static void main(String[] args) {
+//My code		 
+		System.out.println("Welcome to Employee Wage Computation Program");
 
+		Random random = new Random();
+		int attendance = random.nextInt(2);
+		if (attendance ==1 )
+		{
+			System.out.println("Employee is	 present");
+		}
+		else
+		{
+		System.out.println("Employee is not present");	
+
+}
+	}
 }
